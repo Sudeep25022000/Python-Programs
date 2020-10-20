ff0a710c6fcb67cdfeff0802186556971cf42d63
Sudeep Swain <sudeepswain2000@gmail.com>
2020-10-20 20:26:14 +0530

    Create Intro.py

---
+++ Intro.py
@@ -0,0 +1,7 @@
+#print a string
+print('Hello!! Bhubaneswar')
+#input your name
+input('What is your name?')
+#input, store and print your name
+name=input('What is your name?\n')
+print('Hello ' + name)
